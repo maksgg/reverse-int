31c75ce005483b22d5be4a2c7117db3e1b81330c
maksgg <maks.zhovmir.97@gmail.com>
2024-08-22 21:38:27 +0300

    feat:complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    const abs = Math.abs(n);
+    const num = String(abs);
+    let i = 0;
+       
+    while(i <= num.length - 1) {
+      i = i + 1;
+    }
+    let result = num.split('').reverse().join('');
+    const result2 = Number(result);
+    return result2;
 }
